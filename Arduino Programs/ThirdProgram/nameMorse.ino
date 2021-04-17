013963deb661c67aaff978421a4b62d469960c51
Raúl (Station) Pérez <raulytstation@Outlook.com>
2021-04-17 11:00:47 -0400

    Finished nameMorse.ino/ Changed location of nameMorse from /Projects/Arduino Programs/FirstProgram/ to /Projects/Arduino Programs/

---
+++ Arduino Programs/ThirdProgram/nameMorse.ino
@@ -0,0 +1,59 @@
+//nameMorse by stationaryStation/Raul Perez
+//More projects like this on https://github.com/stationaryStation/My-Projects-Repository
+//Ps: You will need to use VSCode with plataformIO for editing and running the program
+void setup()
+{
+    pinMode(13, OUTPUT); //Activates the pin D13 as Output
+}
+
+// morse code: .-. .- ..- .-..
+void loop()
+{
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+    digitalWrite(13,HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(1500);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1500);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1500);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+}
