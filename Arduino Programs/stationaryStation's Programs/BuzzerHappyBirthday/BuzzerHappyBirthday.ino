961146d8e71112386862d78fd9abfee2f1a20a22
Raúl (Station) Pérez <raulytstation@Outlook.com>
2021-04-20 18:38:19 -0400

    Added Pitches.h, toneMelody.ion...

---
+++ Arduino Programs/stationaryStation's Programs/BuzzerHappyBirthday/BuzzerHappyBirthday.ino
@@ -0,0 +1,17 @@
+// BuzzerHappyBirthday by stationaryStation
+// A buzzer is required to run the code. 
+// More of this is available at www.github.com/stationaryStation/My-Projects-Repository/
+//  Coded in VSCODE with PlatformIO and Arduino 1.18.13
+
+void setup()
+{
+    tone(6, 425, 500);
+    delay(600);
+    tone(6, 425, 500);
+    
+}
+
+void loop()
+{
+    
+}
