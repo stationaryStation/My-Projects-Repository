2990cd055f96b837aea698d64f942700a4fd6098
stationaryStation <raulytstation@outlook.com>
2021-05-04 12:34:24 -0400

    Commit #1

---
+++ Arduino Programs/stationaryStation's Programs/herecomesthesun/herecomesthesun.ino
@@ -0,0 +1,44 @@
+#include "pitches.h"
+
+void setup()
+{   
+    tone(6, NOTE_B5, 300);
+    delay(500);
+ tone(6, NOTE_E5, 300);
+ delay(500);
+ tone(6, NOTE_E5, 300);
+ delay(500);
+ tone(6, NOTE_E5, 300); 
+ delay(500);
+ tone(6, NOTE_F5, 700);
+ delay(2500);
+ tone(6, NOTE_E5, 300);
+ delay(500);
+ tone(6, NOTE_B5, 300);
+ delay(500);
+ tone(6, NOTE_E5, 300);
+ delay(500);
+ tone(6, NOTE_E5, 300);
+ delay(500);
+ tone(6, NOTE_E5, 300);
+ delay(500);
+ tone(6, NOTE_F5, 300);
+ delay(500);
+ tone(6, NOTE_G5, 300);
+ delay(500);
+ tone(6, NOTE_A5, 300);
+ delay(500);
+ tone(6, NOTE_G5, 300);
+ delay(500);
+ tone(6, NOTE_E5, 1500);
+ delay(500);
+ tone(6, NOTE_F4, 500);
+ delay(500);
+
+
+}
+
+void loop()
+{
+    
+}
